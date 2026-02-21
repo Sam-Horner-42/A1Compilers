97084353515c33a892da345d3151f8f931d84361
Samuel Horner <Samuelhorner123@gmail.com>
2026-02-21 10:56:51 -0500

    Updated batch file

--- code/run.bat
+++ code/run.bat
@@ -1,10 +1,6 @@
 @echo off
 cd /d "%~dp0"
 
-echo Compiling...
-:: Ensure you use the same files as your Ubuntu command
-gcc *.c -o A1Compilers.exe
-
 echo Encoding...
 :: Added "input/" to the file paths
 A2Compilers.exe 1 1 "Input.stell" "Coded.txt" 
