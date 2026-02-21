8dd0ad0b6ce13129cc806e0f8388d129c0c84808
Samuel Horner <Samuelhorner123@gmail.com>
2026-02-21 10:51:00 -0500

    Configured batch file

--- code/run.bat
+++ code/run.bat
@@ -7,11 +7,13 @@ gcc *.c -o A1Compilers.exe
 
 echo Encoding...
 :: Added "input/" to the file paths
-A2Compilers.exe 1 1 "README.txt" "CODED.txt" 
+A2Compilers.exe 1 1 "Input.stell" "Coded.txt" 
 
 echo Decoding...
 :: Added "input/" to the file paths
-A2Compilers.exe 1 0 "CODED.txt" "RESTORED.txt"
+A2Compilers.exe 1 0 "Coded.stell" "Restored.stell"
 
+echo Running Buffer Reader...
+A2Compilers.exe 2 "Coded.stell"
 echo Done.
 pause
